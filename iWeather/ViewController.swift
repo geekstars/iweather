d53884559de226ee35d2ed39904786114744029f
Hoàng Minh Thành <hoangminhthanh@hoangs-macbook-pro.local>
2016-08-20 15:48:13 +0700

    Initial Commit

---
+++ iWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iWeather
+//
+//  Created by Hoàng Minh Thành on 8/20/16.
+//  Copyright © 2016 Hoàng Minh Thành. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
